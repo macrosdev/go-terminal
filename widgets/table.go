fde5cbb50fac9b0da13cfec841c8ec0624ffe5cd
peter-yang-dev <new.riser@hotmail.com>
2019-02-01 21:05:03 -0800

    Update List example with more scrolling options

--- widgets/table.go
+++ widgets/table.go
@@ -26,8 +26,6 @@ type Table struct {
 	TextStyle    Style
 	RowSeparator bool
 	TextAlign    Alignment
-	RowStyles    map[int]Style
-	FillRow      bool
 }
 
 func NewTable() *Table {
@@ -35,7 +33,6 @@ func NewTable() *Table {
 		Block:        *NewBlock(),
 		TextStyle:    Theme.Table.Text,
 		RowSeparator: true,
-		RowStyles:    make(map[int]Style),
 	}
 }
 
@@ -57,21 +54,9 @@ func (self *Table) Draw(buf *Buffer) {
 	for i := 0; i < len(self.Rows) && yCoordinate < self.Inner.Max.Y; i++ {
 		row := self.Rows[i]
 		colXCoordinate := self.Inner.Min.X
-
-		rowStyle := self.TextStyle
-		// get the row style if one exists
-		if style, ok := self.RowStyles[i]; ok {
-			rowStyle = style
-		}
-
-		if self.FillRow {
-			blankCell := NewCell(' ', rowStyle)
-			buf.Fill(blankCell, image.Rect(self.Inner.Min.X, yCoordinate, self.Inner.Max.X, yCoordinate+1))
-		}
-
 		// draw row cells
 		for j := 0; j < len(row); j++ {
-			col := ParseText(row[j], rowStyle)
+			col := ParseText(row[j], self.TextStyle)
 			// draw row cell
 			if len(col) > columnWidths[j] || self.TextAlign == AlignLeft {
 				for k, cell := range col {
@@ -99,17 +84,9 @@ func (self *Table) Draw(buf *Buffer) {
 		}
 
 		// draw vertical separators
-		separatorStyle := self.Block.BorderStyle
-
 		separatorXCoordinate := self.Inner.Min.X
-		verticalCell := NewCell(VERTICAL_LINE, separatorStyle)
-		for i, width := range columnWidths {
-			if self.FillRow && i < len(columnWidths)-1 {
-				verticalCell.Style.Bg = rowStyle.Bg
-			} else {
-				verticalCell.Style.Bg = self.Block.BorderStyle.Bg
-			}
-
+		verticalCell := NewCell(VERTICAL_LINE, NewStyle(ColorWhite))
+		for _, width := range columnWidths {
 			separatorXCoordinate += width
 			buf.SetCell(verticalCell, image.Pt(separatorXCoordinate, yCoordinate))
 			separatorXCoordinate++
@@ -118,7 +95,7 @@ func (self *Table) Draw(buf *Buffer) {
 		yCoordinate++
 
 		// draw horizontal separator
-		horizontalCell := NewCell(HORIZONTAL_LINE, separatorStyle)
+		horizontalCell := NewCell(HORIZONTAL_LINE, NewStyle(ColorWhite))
 		if self.RowSeparator && yCoordinate < self.Inner.Max.Y && i != len(self.Rows)-1 {
 			buf.Fill(horizontalCell, image.Rect(self.Inner.Min.X, yCoordinate, self.Inner.Max.X, yCoordinate+1))
 			yCoordinate++
